83213f0d524f7e6f066a8df3c977cfdb08457b1a
family <214151436@qq.com>
2020-05-09 11:08:33 +0800

    transaction finished spring extend start

--- src/main/java/com/zhangjie/annotation/ext/ExtConfig.java
+++ src/main/java/com/zhangjie/annotation/ext/ExtConfig.java
@@ -33,11 +33,44 @@ import org.springframework.context.annotation.Configuration;
  *      ApplicationListener<E extends ApplicationEvent> extends EventListener
  *      监听 ApplicationEvent 及其下面的子事件
  *    步骤：
- *        1.写一个监听器来监听某个事件（ApplicationEvent 及其 子类）
+ *        1.写一个监听器（ApplicationEvent的实现类）来监听某个事件（ApplicationEvent 及其 子类）
+ *          或者使用 @EventListener 注解
+ *              原理：使用 EventListenerMethodProcessor 处理器来解析方法上的 @EventListener 注解
  *        2.把监听器加入到容器中
  *        3.只要容器中有相关事件的发布，就能监听到这个事件
  *          ContextRefreshedEvent：容器刷新完成（所有bean都完全创建）会发布这个事件
  *        4.发布一个事件
+ *    原理：
+ *        ContextRefreshedEvent、AnnotationExt$1[source=我发布的事件]、ContextClosedEvent
+ *        1.ContextRefreshedEvent 事件：
+ *            1.容器创建对象：refresh();
+ *            2.finishRefresh() 容器刷新完成，容器刷新完成会发布ContextRefreshEvent 事件
+ *            3.publishEvent(new ContextRefreshContext(this))
+ *              发布流程：获取到事件的多播器（派发器）：getApplicationEventMulticaster()
+ *              multicastEvent 派发事件
+ *              获取到所有的监听器 ApplicationListener
+ *                  for (final ApplicationListener<?> listener : getApplicationListeners(event, type))
+ *                  如果有 Executor 可以支持使用 Executor 进行异步派发
+ *                      Executor executor = getTaskExecutor();
+ *                  否则 同步的方式 直接执行 listener 方法
+ *                      invokeListener(listener, event); 回调 onApplicationEvent(ApplicationEvent event) 方法
+ *        2.自己发布事件（直接走1.3流程）
+ *        3.容器关闭会发布 ContextClosedEvent 事件
+ *    事件多播器：
+ *        1.容器创建对象：调用 refresh() 方法
+ *        2.initApplicationEventMulticaster() 初始化 ApplicationEventMulticaster
+ *          先去容器中找有没有id=“applicationEventMulticaster”的组件，
+ *          如果没有就创建一个SimpleApplicationEventMulticaster，并且加入到容器中，就可以在其他组件要派发事件的时候自动注入 applicationEventMulticaster 组件
+ *    容器中有哪些监听器：
+ *        1.事件多播器注册完成以后调用 registerListeners(); 方法获取到所用的 ApplicationListener 组件，并注册到多播器中 （getApplicationEventMulticaster().addApplicationListenerBean(listenerBeanName);）
+ *
+ *    SmartInitializingSingleton 原理：
+ *        1.ioc容器创建对象，refresh()刷新容器
+ *        2.finishBeanFactoryInitialization(beanFactory); 初始化剩下的单实例bean
+ *          1.先创建所有的单实例bean，调用 getBean()方法来创建
+ *          2.获取所有创建好的单实例bean，判断是否是实现了 SmartInitializingSingleton 接口的bean
+ *              如果是 调用 afterSingletonsInstantiated() 方法
+ *
  *
  */
 @ComponentScan(value = {"com.zhangjie.annotation.ext"})
--- src/test/java/com/zhangjie/springstudy/AnnotationExt.java
+++ src/test/java/com/zhangjie/springstudy/AnnotationExt.java
@@ -10,6 +10,6 @@ public class AnnotationExt {
     public void test01(){
         AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext(ExtConfig.class);
         applicationContext.publishEvent(new ApplicationEvent("我发布的事件") {});
-
+        applicationContext.close();
     }
 }
